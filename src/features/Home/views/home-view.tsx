11b91fa8cf71d31a5e7eb4feadc356bc66d496b4
KONE HADOU <hadoukone0102@gmail.com>
2026-02-23 16:58:30 +0000

    initialization du projet

---
+++ src/features/Home/views/home-view.tsx
@@ -0,0 +1,7 @@
+export default function HomePage (){
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h3 className="text-bold text-4xl italic">Bienvenue sur le site de web de corrick group</h3>
+        </div>
+    )
+}
